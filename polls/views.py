e1407ecec83132eeb297086e57d95e2eb030064e
Kimmy <kimmy.liu@cgptalent.com>
2021-07-01 19:13:33 +0800

    generic view

--- polls/views.py
+++ polls/views.py
@@ -2,17 +2,17 @@ from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request: HttpRequest):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 def detail(request: HttpRequest, question_id: int):
