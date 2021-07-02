359180bb1669ad692ff11bf76abcad6faf100ce3
Kimmy <kimmy.liu@cgptalent.com>
2021-07-02 16:02:30 +0800

    add playmodelquery app

--- djworks/settings.py
+++ djworks/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'playmodel',
+    'playmodelquery',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ playmodelquery/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlaymodelqueryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'playmodelquery'
