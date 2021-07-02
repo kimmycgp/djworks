af1f043acd1bdfdb6ffec5886e0ce93205598dcd
Kimmy <kimmy.liu@cgptalent.com>
2021-07-02 15:39:05 +0800

    many to many relationship

--- playmodel/models.py
+++ playmodel/models.py
@@ -22,3 +22,15 @@ class Person(models.Model):
     )
     name = models.CharField(max_length=60)
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZE)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
