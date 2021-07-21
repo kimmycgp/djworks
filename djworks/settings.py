7f9848d99216d72214c851ec423a67f80e158d78
Kimmy <kimmy.liu@cgptalent.com>
2021-07-21 18:37:55 +0800

    add channels

--- djworks/settings.py
+++ djworks/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'djworks.asgi.application'
